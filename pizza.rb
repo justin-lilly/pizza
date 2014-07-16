58ba53f1f55203228066ed3d8c0918d36df921b9
justin-lilly <jlilly353@gmail.com>
2014-07-16 11:37:25 -0500

    Pass initialize defualt array test

--- pizza.rb
+++ pizza.rb
@@ -1,24 +1,22 @@
 class Pizza
+  attr_reader :toppings
 
   def initialize(toppings_arr=nil)
-     @toppings = [];
-     add_topping(toppings_arr)
+     @toppings = []
+     add_topping(toppings_arr);
   end
 
 
   def add_topping(toppings_arr)
     if toppings_arr.nil?
-      @toppings << Topping.new()
+      @toppings.push(Topping.new('cheese'))
     else
       toppings_arr.each do |i| 
-        @toppings << Topping.new(i)
-      end
+        @toppings.push(i)
+        end
      end
   end
 
-  def toppings
-    @toppings.map{ |i| i.name }
-  end
       
 end
 
@@ -27,7 +25,7 @@ class Topping
 
   attr_reader :name, :vegetarian
 
-  def initialize (name = 'cheese', vegetarian: false)
+  def initialize (name , vegetarian: false)
     @name = name
     @vegetarian = vegetarian
   end
